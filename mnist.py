a41db42d766d094187920997c2ef3b8243937c43
ryowork2 <ryowork2@gmail.com>
2024-11-24 14:55:42 +0900

    first commit

--- mnist.py
+++ mnist.py
@@ -21,7 +21,7 @@ app.secret_key = "your_secret_key_here"
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-model = load_model('./model.h5')#学習済みモデルをロード
+model = load_model('./model.h5', compile=False)
 
 
 @app.route('/', methods=['GET', 'POST'])
